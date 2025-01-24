905ff65f98d8266d49c04d3b88b151116b6ae2bf
shorte1 <165932533+shorte1@users.noreply.github.com>
2025-01-23 22:41:27 -0600

    wtf is the difference between an int object and an int

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = dash.Dash(__name__)
 num = 100
 range = 100
 values = []
-for x in range(num):
+for x in range(100):
     values.append([np.random.randint(0, range), np.random.randint(0, range)])
 
 # App layout
