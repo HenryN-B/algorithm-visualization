d837803962422493d69e325a0f60a6b97a5ecedb
Henry <henrynorton-bower@Henry-Mac-Air.local>
2025-01-26 10:31:13 -0600

    i made it so the hull calculations happen in a function and edited the look of the page

--- app.py
+++ app.py
@@ -10,25 +10,51 @@ import numpy as np
 import gram_scan as gs
 import time
 
+
+
+def graham_scan(set):
+    start_time = time.perf_counter()
+    hull = gs.graham_scan(set)
+    end_time = time.perf_counter()
+    total_time = end_time-start_time
+    x = []
+    y = []
+    for i in hull:
+        x.append(i[0])
+        y.append(i[1])
+    x.append(hull[0][0])
+    y.append(hull[0][1])
+    pX = []
+    pY = []
+    for i in set:
+        pX.append(i[0])
+        pY.append(i[1])
+    return hull, total_time, x, y, pX, pY
+
+def update_graham_scan(set,x,y):
+    set.append([x,y])
+    start_time = time.perf_counter()
+    hull = gs.graham_scan(set)
+    end_time = time.perf_counter()
+    total_time = end_time-start_time
+    newx = []
+    newy = []
+    for i in hull:
+        newx.append(i[0])
+        newy.append(i[1])
+    newx.append(hull[0][0])
+    newy.append(hull[0][1])
+    pX = []
+    pY = []
+    for i in set:
+        pX.append(i[0])
+        pY.append(i[1])
+    return newx, newy, pX, pY, total_time
+
+
+
 set = gs.random_points(100, 100)
-
-start_time = time.perf_counter()
-hull = gs.graham_scan(set)
-end_time = time.perf_counter()
-total_time = end_time-start_time
-x = []
-y = []
-for i in hull:
-    x.append(i[0])
-    y.append(i[1])
-x.append(hull[0][0])
-y.append(hull[0][1])
-pX = []
-pY = []
-for i in set:
-    pX.append(i[0])
-    pY.append(i[1])
-
+hull, total_time, x, y, pX, pY = graham_scan(set)
 fig = px.imshow(np.zeros(shape=(120, 120, 4)), origin='lower')
 fig.add_scatter(x=x, y=y, mode='lines+markers',marker_color='white',marker_size=8)
 fig.add_scatter(x=pX, y=pY, mode='markers',marker_color='white',marker_size=4)
@@ -64,35 +90,57 @@ app = Dash(
     ]
 )
 
-# app layout
 app.layout = dbc.Container(
     [
         dbc.Row(
             dbc.Col(
-                dcc.Graph(
-                    id='graph',
-                    figure=fig,
-                    config={
-                        'scrollZoom': True,
-                        'displayModeBar': False,
-                    }
-                ),
-                width={'size': 5, 'offset': 0}
-            ), justify='around'
-        ),
-        dbc.Row(
-            dbc.Col(
-                html.Div(
-                    id='runtime_display',  
-                    children= f"Runtime for Gram Scan: {total_time*1000} milliseconds",
-                    style={'textAlign': 'center', 'marginTop': '10px'}
-                ),
-                width={'size': 5, 'offset': 0}
-            ), justify='around'
+                [
+                    # really cool graph
+                    dcc.Graph(
+                        id='graph',
+                        figure=fig,
+                        config={
+                            'scrollZoom': True,
+                            'displayModeBar': False,
+                        }
+                    ),
+                    # Button
+                    dbc.Button(
+                        "Re-run with New Points",
+                        id='rerun-button',
+                        color='primary',
+                        className='shadow-sm my-3',  
+                        n_clicks=0
+                    ),
+                    # Runtime 
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.P(
+                                    f"Runtime: {total_time*1000:.2f} ms",
+                                    className="card-text",
+                                    style={
+                                        'textAlign': 'left',  
+                                        'fontSize': '20px'  
+                                    }
+                                )
+                            ]
+                        ),
+                        className="shadow-sm mt-3", 
+                        style={
+                            'padding': '5px', 
+                            'width': '250px'
+                        }
+                    )
+                ],
+                width=8  
+            ),
+            justify='center',   
         )
-    ], fluid=True
+    ],
+    fluid=True,
+    className="px-4 py-3"
 )
-
 
 @ app.callback(
     Output('graph', 'figure'),
@@ -106,24 +154,10 @@ def get_click(graph_figure, clickData):
         points = clickData.get('points')[0]
         x = points.get('x')
         y = points.get('y')
-
         # get scatter trace (in this case it's the last trace)
-        set.append([x,y])
-        hull = gs.graham_scan(set)
-        newx = []
-        newy = []
-        for i in hull:
-            newx.append(i[0])
-            newy.append(i[1])
-        newx.append(hull[0][0])
-        newy.append(hull[0][1])
-        pX = []
-        pY = []
-        for i in set:
-            pX.append(i[0])
-            pY.append(i[1])
+        newx, newy, pX, pY,total_time = update_graham_scan(set,x,y)
+        
         # fig.add_scatter(x=newx, y=newy, mode='lines+markers')
-
         # update figure data (in this case it's the last trace)
         graph_figure['data'][1].update(x=newx)
         graph_figure['data'][1].update(y=newy)
