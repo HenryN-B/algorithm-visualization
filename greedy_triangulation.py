ca78dc33acd015a251c22de2a353d04ffd365e04
Will Anderson <ander39@stolaf.edu>
2025-01-28 14:24:07 -0600

    Greedy triangulation partially complete. The intersect function is not tested but should work. left_of function in gram scan may not be true gift wrap algorithm.

--- greedy_triangulation.py
+++ greedy_triangulation.py
@@ -18,12 +18,13 @@ def left_of(a,b,c):
     elif q <= 0:
         return False
 
-def intersectQuerie([[a1,b1],[b1,b2]][[s1,s2],[t1,t2]]):
+def intersectQuerie([[a1,a2],[b1,b2]][[s1,s2],[t1,t2]]):
     a_left_of_st = left_of([s1,s2],[t1,t2],[a1,a2])
     b_left_of_st = left_of([s1,s2],[t1,t2],[b1,b2])
     s_left_of_ab = left_of([a1,a2],[b1,b2],[s1,s2])
     t_left_of_ab = left_of([a1,a2],[b1,b2],[t1,t2])
-    if a_left_of_st != b_left_of_st and s_left_of_st != t_left_of_ab: #the lines should only intersect if this is true
+    #st and ab only intersect if this is a and b are on opposite sides of st and s and t are on opposite sides of ab
+    if a_left_of_st != b_left_of_st and s_left_of_st != t_left_of_ab: 
         return True
     
     return False    #returns false if the lines don't intersect
@@ -31,6 +32,6 @@ def intersectQuerie([[a1,b1],[b1,b2]][[s1,s2],[t1,t2]]):
 def greedyTriangulationAlgorithm(points):
     triangulationEdges = [[[],[]]]
     numVertices = len(points)
-#    for i in
+    for i in numVertices
 
     
