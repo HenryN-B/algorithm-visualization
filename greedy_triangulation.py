985efabebfcfbda864820ba80cd57f1e38f61a46
Will Anderson <ander39@stolaf.edu>
2025-01-28 14:21:17 -0600

    updated intersect querie to simplify. also started the greedy triangulation algorithm that includes the format which we will use to store the edges of the triangulations.

--- greedy_triangulation.py
+++ greedy_triangulation.py
@@ -23,10 +23,14 @@ def intersectQuerie([[a1,b1],[b1,b2]][[s1,s2],[t1,t2]]):
     b_left_of_st = left_of([s1,s2],[t1,t2],[b1,b2])
     s_left_of_ab = left_of([a1,a2],[b1,b2],[s1,s2])
     t_left_of_ab = left_of([a1,a2],[b1,b2],[t1,t2])
-    if a_left_of_st == b_left_of_st:
-        return False    #a and b are on the same side of st: ab does not intersect st
-    elif a_left_of_st != b_left_of_st and s_left_of_st != t_left_of_ab:
+    if a_left_of_st != b_left_of_st and s_left_of_st != t_left_of_ab: #the lines should only intersect if this is true
         return True
-    else:
-        return False
+    
+    return False    #returns false if the lines don't intersect
+
+def greedyTriangulationAlgorithm(points):
+    triangulationEdges = [[[],[]]]
+    numVertices = len(points)
+#    for i in
+
     
