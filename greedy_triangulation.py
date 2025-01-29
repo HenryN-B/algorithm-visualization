3f32b5404f952d0606a76397afd3e77dd353c66d
Will Anderson <ander39@stolaf.edu>
2025-01-29 11:40:59 -0600

    intersect querie should be fixed now

--- greedy_triangulation.py
+++ greedy_triangulation.py
@@ -18,11 +18,11 @@ def left_of(a,b,c):
     elif q <= 0:
         return False
 
-def intersectQuerie([[a1,a2],[b1,b2]][[s1,s2],[t1,t2]]):
-    a_left_of_st = left_of([s1,s2],[t1,t2],[a1,a2])
-    b_left_of_st = left_of([s1,s2],[t1,t2],[b1,b2])
-    s_left_of_ab = left_of([a1,a2],[b1,b2],[s1,s2])
-    t_left_of_ab = left_of([a1,a2],[b1,b2],[t1,t2])
+def intersectQuerie(a,b,s,t):
+    a_left_of_st = left_of([s[0],s[1]],[t[0],t[1]],[a[0],a[1]])
+    b_left_of_st = left_of([s[0],s[1]],[t[0],t[1]],[b[0],b[1]])
+    s_left_of_ab = left_of([a[0],a[1]],[b[0],b[1]],[s[0],s[1]])
+    t_left_of_ab = left_of([a[0],a[1]],[b[0],b[1]],[t[0],t[1]])
     #st and ab only intersect if this is a and b are on opposite sides of st and s and t are on opposite sides of ab
     if a_left_of_st != b_left_of_st and s_left_of_st != t_left_of_ab: 
         return True
@@ -32,6 +32,7 @@ def intersectQuerie([[a1,a2],[b1,b2]][[s1,s2],[t1,t2]]):
 def greedyTriangulationAlgorithm(points):
     triangulationEdges = [[[],[]]]
     numVertices = len(points)
-    for i in numVertices
+    for i in numVertices:
+
 
     
